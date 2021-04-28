5e1ef5f964fee519dc9ba4772327f973072e4e1b
Karanbir Singh Janjua <karanbirjanjua@gmail.com>
2021-04-28 12:42:07 +0930

    Adding dummie customer list and new Randomviewmodel class

--- Vidley/Controllers/MoviesController.cs
+++ Vidley/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidley.Models;
+using Vidley.ViewModels;
 
 namespace Vidley.Controllers
 {
@@ -17,13 +18,31 @@ namespace Vidley.Controllers
             {
                 Name = "Shrek!"
             };
-            return View(movie);
+
+            var Customers = new List<Customer>
+            {
+
+                new Customer { Name= "Customer 1"},
+                new Customer { Name = "Customer 2" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = Customers
+
+            };
+            return View(viewModel);
         }
 
+
+        /*
         [Route("movies/released/{year}/{month}")]
         public ActionResult ByReleaseDate(int year, int month)
         {
             return Content(year+"/"+month);
         }
-    }
+    
+        */
+        }
 }
